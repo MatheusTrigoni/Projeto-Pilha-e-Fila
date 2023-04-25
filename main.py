5c7f5f5a1d28e3cccb5893b721c6e9ed53b94182
MatheusTrigoni <matheusbr360@gmail.com>
2023-04-25 15:23:27 -0300

    Algumas alterações,  correções e adição da README

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from time import sleep
 
 def menu_principal():
     system("cls")
-    print("\nMENU PRINCIPAL\n1 - Operações\n2 - Expressão\n0 - Finalizar Programa")
+    print("MENU PRINCIPAL\n1 - Operações\n2 - Expressão\n0 - Finalizar Programa")
     # Acredito ser o equilíbrio entre ter as informações mantidas sem perder tudo do nada e não deixar o terminal extremamente poluído
     print("\nOBS: Quando uma ou mais operações forem realizadas no menu de OPERAÇÕES, elas serão apagadas da fila e do terminal (funcional e visualmente).")
     escolha = input()
@@ -13,125 +13,128 @@ def menu_principal():
         operações(filaTotal)
     elif escolha == '0':
         system("cls")
-        print("\nPrograma finalizado.\nVolte sempre, obrigado!\n")
+        print("Programa finalizado.\nVolte sempre, obrigado!\n")
         return
     else:
+        system("cls")
         print("Opção inválida, retornado ao MENU PRINCIPAL...")
         sleep(2)
         menu_principal()
 
 def operações(ft: deque):
     system("cls") 
-    print("\nOPERAÇÕES\n1 - Adicionar Operação na Fila\n2 - Executar Próxima Operação da Fila")
+    print("OPERAÇÕES\n1 - Adicionar Operação à Fila\n2 - Executar Próxima Operação da Fila")
     print("3 - Executar Todas as Operações da Fila\n0 - Voltar para o MENU PRINCIPAL")
     escolha = input()
     if escolha == '1':
         system("cls")
-        print("\n1 - Adição (+)\n2 - Subtração (-)\n3 - Multiplicação (*)\n4 - Divisão (/)")
+        print("1 - Adição (+)\n2 - Subtração (-)\n3 - Multiplicação (*)\n4 - Divisão (/)")
         escolha = input()
         # Essa verificação existe aqui para que não haja necessidade de verificar durante a execução das operações
         if escolha not in ('1', '2', '3', '4'):
-            print("\nOperação inválida, retornando ao menu de OPERAÇÕES...")
+            system("cls")
+            print("Operação inválida, retornando ao menu de OPERAÇÕES...")
             sleep(2)
             operações(filaTotal)
-        system("cls")
-        print('\nDigite quantos valores quiser e "fim" para encerrar a inserção de valores:')
-        fila = deque([int(escolha)])
-        while True:
-            valor = input()
-            if valor == "fim":
-                break
-            else:
-                try:
-                    fila.append(float(valor))
-                except ValueError:
-                    print('O valor inserido não se trata de um valor numérico (lembre-se, para encerrar a inserção, digite "fim").')
-        ft.append(fila)
-        system("cls")
-        operações(filaTotal)
-    elif escolha == '2':
-        # Necessário para caso o usuário selecione para executar a próxima operação na fila quando a mesma estiver vazia
-        try:
-            if ft[0][0] == 1:
-                system("cls")
-                adição(ft[0])
-            elif ft[0][0] == 2:
-                system("cls")
-                subtração(ft[0])
-            elif ft[0][0] == 3:
-                system("cls")
-                multiplicação(ft[0])
-            else:
-                system("cls")
-                divisão(ft[0])
-            ft.popleft()
-            input("\nEsses dados não são armazenadas em lugar algum, dê uma boa olhada neles antes que desapareçam. Aperte ENTER ")
-            operações(filaTotal)
-        except IndexError:
+        else:
             system("cls")
-            print("\nA fila de operações está vazia, retornando ao menu de OPERAÇÕES...")
-            sleep(2)
-            operações(filaTotal)
-    elif escolha == '3':
-        try:
-            a = ft[0]
-            del a
-            system("cls")
-            for i in ft:
-                if i[0] == 1:
-                    adição(i)
-                elif i[0] == 2:
-                    subtração(i)
-                elif i[0] == 3:
-                    multiplicação(i)
+            print('Digite quantos valores quiser e "fim" para encerrar a inserção de valores:')
+            fila = deque([int(escolha)])
+            while True:
+                valor = input()
+                if valor == "fim":
+                    break
                 else:
-                    divisão(i)
-            ft.clear()
-            input("\nEsses dados não são armazenadas em lugar algum, dê uma boa olhada neles antes que desapareçam. Aperte ENTER ")
+                    try:
+                        fila.append(float(valor))
+                    except ValueError:
+                        print('O valor inserido não se trata de um valor numérico (lembre-se, para encerrar a inserção, digite "fim").')
+            ft.append(fila)
+            system("cls")
+            operações(filaTotal)
+    elif escolha == '2':
+        system("cls")
+        manipulação_fila(ft)
+        # Contexto do bug referenciado na README abaixo
+        try:
+            ft.popleft()
+            input("Esses dados não são armazenadas em lugar algum, dê uma boa olhada neles antes que desapareçam. Aperte ENTER ")
             operações(filaTotal)
         except IndexError:
+            pass
+    elif escolha == '3':
+        system("cls")
+        if len(ft) > 0:
+            i = 0
+            while i < len(ft):
+                manipulação_fila(ft, i) # Passando a fila total e o index da "subfila" como parâmetros
+                i += 1
+            ft.clear()
+            input("Esses dados não são armazenadas em lugar algum, dê uma boa olhada neles antes que desapareçam. Aperte ENTER ")
+            operações(filaTotal)
+        else:
             system("cls")
-            print("\nA fila de operações está vazia, retornando ao menu de OPERAÇÕES...")
+            print("A fila de operações está vazia, retornando ao menu de OPERAÇÕES...")
             sleep(2)
             operações(filaTotal)
     elif escolha == '0':
         menu_principal()
+    else:
+        system("cls")
+        print("Opção inválida, retornando ao menu de OPERAÇÕES...")
+        sleep(2)
+        operações(filaTotal)
+
+def manipulação_fila(filaInteira: deque, index=0):
+    try: # Necessário para caso a fila estiver vazia
+        if filaInteira[index][0] == 1:
+            adição(filaInteira[index])
+        elif filaInteira[index][0] == 2:
+            subtração(filaInteira[index])
+        elif filaInteira[index][0] == 3:
+            multiplicação(filaInteira[index])
+        else:
+            divisão(filaInteira[index])
+    except IndexError:
+        print("A fila de operações está vazia, retornando ao menu de OPERAÇÕES...")
+        sleep(2)
+        operações(filaTotal)
 
 def adição(filaOp: deque):
-    print("\nAdição\nValores:", end=' ')
+    print("Adição\nValores:", end=' ')
     filaOp.popleft() # Tirando o inteiro que representa a operação
     for i in filaOp:
         print(i, end=" | ")
-    print("\nResultado:", sum(filaOp))
+    print("\nResultado:", sum(filaOp), "\n")
     
 def subtração(filaOp: deque):
-    print("\nSubtração\nValores:", end=' ')
+    print("Subtração\nValores:", end=' ')
     filaOp.popleft() # Tirando o inteiro que representa a operação
     sub = filaOp.popleft() # Tirando o primeiro número da operação e o atribuindo a sub para meios de cálculo
     print(sub, end=" | ") # Printando esse único valor fora do loop (já que ele "não faz mais parte da lista")
     for i in filaOp:
         print(i, end=" | ")
         sub -= i
-    print("\nResultado:", sub)
+    print("\nResultado:", sub, "\n")
 
 def multiplicação(filaOp: deque):
-    print("\nMultiplicação\nValores:", end=' ')
+    print("Multiplicação\nValores:", end=' ')
     filaOp.popleft() # Tirando o inteiro que representa a operação
     multi = 1
     for i in filaOp:
         print(i, end=" | ")
         multi *= i
-    print("\nResultado:", multi)
+    print("\nResultado:", multi, "\n")
 
 def divisão(filaOp: deque):
-    print("\nDivisão\nValores:", end=' ')
+    print("Divisão\nValores:", end=' ')
     filaOp.popleft() # Tirando o inteiro que representa a operação
     div = filaOp.popleft() # Tirando o primeiro número da operação e o atribuindo a div para meios de cálculo
     print(div, end=" | ") # Printando esse único valor fora do loop (já que ele "não faz mais parte da lista")
     for i in filaOp:
         print(i, end=" | ")
         div /= i
-    print("\nResultado:", div)
+    print("\nResultado:", div, "\n")
 
 filaTotal = deque()
 menu_principal()
