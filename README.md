7455c4fa99b60c8fb6108d02a45771fa3e4d96b2
MatheusTrigoni <matheusbr360@gmail.com>
2023-04-26 15:51:05 -0300

    Alterações para utilização da fila FIFO propriamente

--- README.md
+++ README.md
@@ -10,7 +10,8 @@ Consiste no uso dos conceitos de Pilha e Fila em um programa que funciona como c
 ### Menu Principal
 
 1. Ir para o menu de Operações.
-2. Finalizar o programa.
+2. Validar expressão matemática (desenvolvimento ainda não iniciado)
+3. Finalizar o programa.
 
 ### Menu de Operações
 
@@ -18,9 +19,3 @@ Consiste no uso dos conceitos de Pilha e Fila em um programa que funciona como c
 2. Executar a próxima operação da fila.
 3. Executar todas as operações da fila.
 4. Retornar ao Menu Principal.
-
-## Observação
-
-Na linha 57 da main.py foi necessário o uso de um try-except statement, usá-lo foi a maneira mais simples de corrigir um bug encontrado
-quando a fila estava vazia: em ambas escolhas 2 e 3, a função operações() aparentava ser chamada duas vezes seguidas (mesmo tendo apenas
-uma chamada no script) e(ou), na primeira chamada, caía automaticamente no escopo da opção 2.
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from collections import deque
+from queue import Queue
 from queue import LifoQueue
 from time import sleep
 from os import system
@@ -6,6 +6,7 @@ from os import system
 def menu_principal():
     system("cls")
     print("MENU PRINCIPAL\n1 - Operações\n2 - Expressão\n0 - Finalizar Programa")
+    print("\nOBS: Quando uma ou mais operações forem realizadas no menu de OPERAÇÕES, elas serão apagadas da fila e do terminal (funcional e visualmente).")
     escolha = input()
     if escolha == '1':
         operações(filaTotal)
@@ -15,11 +16,11 @@ def menu_principal():
         return
     else:
         system("cls")
-        print("Opção inválida, retornado ao menu principal...")
+        print("Opção inválida, retornado ao MENU PRINCIPAL...")
         sleep(1)
         menu_principal()
 
-def operações(ft: deque):
+def operações(ft: Queue):
     system("cls")
     print("OPERAÇÕES\n1 - Adicionar Operação à Fila\n2 - Executar Próxima Operação da Fila")
     print("3 - Executar Todas as Operações da Fila\n0 - Voltar para o menu principal")
@@ -31,42 +32,42 @@ def operações(ft: deque):
         # Essa verificação existe aqui para que não haja necessidade de verificar durante a execução das operações
         if escolha not in ('1', '2', '3', '4'):
             system("cls")
-            print("Operação inválida, retornando ao menu de operações...")
+            print("Operação inválida, retornando ao menu de OPERAÇÕES...")
             sleep(1)
             operações(filaTotal)
         else:
             system("cls")
             print('Digite quantos valores quiser e "fim" para encerrar a inserção de valores:')
-            fila = deque([int(escolha)])
+            fila = Queue()
+            fila.put(int(escolha))
             while True:
                 valor = input()
                 if valor == "fim":
                     break
                 else:
                     try:
-                        fila.append(float(valor))
+                        fila.put(float(valor))
                     except ValueError:
                         print('O valor inserido não se trata de um valor numérico (lembre-se, para encerrar a inserção, digite "fim").')
-            ft.append(fila)
+            if fila.qsize() > 2: # A fila precisa ter pelo menos 2 valores para que uma operação se realize (o primeiro sendo a representação da operação em si)
+                ft.put(fila)
             operações(filaTotal)
     elif escolha == '2':
         system("cls")
-        manipulação_fila(ft)
-        # Contexto do bug referenciado na README abaixo
-        try:
-            ft.popleft()
+        if not ft.empty():
+            manipulação_fila(ft.get())
             input("Esses dados não são armazenadas em lugar algum, dê uma boa olhada neles antes que desapareçam. Aperte ENTER ")
             operações(filaTotal)
-        except IndexError:
-            pass
+        else:
+            print("A fila está vazia, retornando ao menu de OPERAÇÕES...")
+            sleep(2)
+            operações(filaTotal)
     elif escolha == '3':
         system("cls")
-        if len(ft) > 0:
-            i = 0
-            while i < len(ft):
-                manipulação_fila(ft, i) # Passando a fila total e o index da "subfila" como parâmetros
-                i += 1
-            ft.clear()
+        if not ft.empty():
+            while ft.qsize() > 0:
+                manipulação_fila(ft.get())
+                sleep(0.5)
             input("Esses dados não são armazenadas em lugar algum, dê uma boa olhada neles antes que desapareçam. Aperte ENTER ")
             operações(filaTotal)
         else:
@@ -82,56 +83,57 @@ def operações(ft: deque):
         sleep(2)
         operações(filaTotal)
 
-def manipulação_fila(filaInteira: deque, index=0):
-    try: # Necessário para caso a fila estiver vazia
-        if filaInteira[index][0] == 1:
-            adição(filaInteira[index])
-        elif filaInteira[index][0] == 2:
-            subtração(filaInteira[index])
-        elif filaInteira[index][0] == 3:
-            multiplicação(filaInteira[index])
-        else:
-            divisão(filaInteira[index])
-    except IndexError:
-        print("A fila de operações está vazia, retornando ao menu de OPERAÇÕES...")
-        sleep(2)
-        operações(filaTotal)
+def manipulação_fila(primeiraFila: Queue):
+    op = primeiraFila.get() # Atribuindo o primeiro número da fila (que representa a operação) à variável op
+    if op == 1:
+        adição(primeiraFila)
+    elif op == 2:
+        subtração(primeiraFila)
+    elif op == 3:
+        multiplicação(primeiraFila)
+    else:
+        divisão(primeiraFila)
 
-def adição(filaOp: deque):
+def adição(filaOp: Queue):
     print("Adição\nValores:", end=' ')
-    filaOp.popleft() # Tirando o inteiro que representa a operação
-    for i in filaOp:
-        print(i, end=" | ")
-    print("\nResultado:", sum(filaOp), "\n")
+    soma = 0
+    while not filaOp.empty():
+        i = filaOp.get()
+        print(i, end=" | " if 0 < filaOp.qsize() else '') # Apenas para fins estéticos
+        soma += i
+    print("\nResultado:", soma, "\n")
     
-def subtração(filaOp: deque):
+def subtração(filaOp: Queue):
     print("Subtração\nValores:", end=' ')
-    filaOp.popleft() # Tirando o inteiro que representa a operação
-    sub = filaOp.popleft() # Tirando o primeiro número da operação e o atribuindo a sub para meios de cálculo
-    print(sub, end=" | ") # Printando esse único valor fora do loop (já que ele "não faz mais parte da lista")
-    for i in filaOp:
-        print(i, end=" | ")
+    sub = i = filaOp.get() # Tirando o primeiro número da operação e o atribuindo a sub e i para meios de cálculo
+    print(i, end=" | ") # Printando esse único valor fora do loop pois será posteriormente manipulado
+    while not filaOp.empty():
+        i = filaOp.get()
+        print(i, end=" | " if 0 < filaOp.qsize() else '') # Apenas para fins estéticos
         sub -= i
     print("\nResultado:", sub, "\n")
 
-def multiplicação(filaOp: deque):
+def multiplicação(filaOp: Queue):
     print("Multiplicação\nValores:", end=' ')
-    filaOp.popleft() # Tirando o inteiro que representa a operação
     multi = 1
-    for i in filaOp:
-        print(i, end=" | ")
+    while not filaOp.empty():
+        i = filaOp.get()
+        print(i, end=" | " if 0 < filaOp.qsize() else '') # Apenas para fins estéticos
         multi *= i
     print("\nResultado:", multi, "\n")
 
-def divisão(filaOp: deque):
+def divisão(filaOp: Queue):
     print("Divisão\nValores:", end=' ')
-    filaOp.popleft() # Tirando o inteiro que representa a operação
-    div = filaOp.popleft() # Tirando o primeiro número da operação e o atribuindo a div para meios de cálculo
-    print(div, end=" | ") # Printando esse único valor fora do loop (já que ele "não faz mais parte da lista")
-    for i in filaOp:
-        print(i, end=" | ")
-        div /= i
+    div = i = filaOp.get() # Tirando o primeiro número da operação e o atribuindo a div e i para meios de cálculo
+    print(i, end=" | ") # Printando esse único valor fora do loop pois será posteriormente manipulado
+    while not filaOp.empty():
+        i = filaOp.get()
+        print(i, end=" | " if 0 < filaOp.qsize() else '') # Apenas para fins estéticos
+        try:
+            div /= i
+        except ZeroDivisionError:
+            pass # Apenas ignorando o valor 0 digitado
     print("\nResultado:", div, "\n")
 
-filaTotal = deque()
+filaTotal = Queue()
 menu_principal()
